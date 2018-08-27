1604f6d70d74c5fb387e2c1a8140b88c10096c5e
Cody Mitchell <codyshanemitchell@gmail.com>
2018-08-27 10:27:05 -0700

    constructor function creation for resources and users

--- js/app.js
+++ js/app.js
@@ -1,6 +1,50 @@
 'use strict';
 
-//constructor function to create resource objects
-// var Resource = function {
 
-// }
+var resources = [];
+var users = [];
+
+//constructor function to create resource objects
+function Resource(name, url, phone, address,zipCode, shelter, food, drugAlcohol, mentalTherapy, women, men, menOverSixty, children) {
+  this.name = name;
+  this.url = url;
+  this.phone = phone;
+  this.address = address;
+  this.zipCode = zipCode;
+
+  //Booleans Below!
+  this.shelter = shelter;
+  this.food = food;
+  this.drugAlcohol = drugAlcohol;
+  this.mentalTherapy = mentalTherapy;
+  this.allWomen = women;
+  this.allMen = men;
+  this.menOverSixty = menOverSixty;
+  this.children = children;
+
+  //Pushing into resources array
+  resources.push(this);
+}
+//Resource Object Creations Live here
+new Resource('TherapyTest Center', 'www.therapyTest.com','555-555-5555', 'some address dr.', true, false, true, true, false, true, false, true);
+
+
+function User(name, gender, age, children, shelter, food, drugAlcohol, mentalTherapy) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.children = children;
+  this.shelter = shelter;
+  this.food = food;
+  this.drugAlcohol = drugAlcohol;
+  this.mentalTherapy = mentalTherapy;
+}
+
+//
+function listAllResources() {
+  for(var i = 0; i < resources.length; i++) {
+    console.log(resources[i]);
+  }
+}
+
+listAllResources();
